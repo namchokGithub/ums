1bc447e0be233c11499fd3358617609031e4af42
Namchok Singhachai <44488002+namchokGithub@users.noreply.github.com>
2020-08-26 15:53:21 +0700

    add dependencies email service

---
+++ EmailService/Message.cs
@@ -0,0 +1,24 @@
+﻿using MimeKit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EmailService
+{
+    public class Message
+    {
+        public List<MailboxAddress> To { set; get; }
+        public string Subject { set; get; }
+        public string Content { set; get; }
+
+        public Message(IEnumerable<string> to, string subject, string content)
+        {
+            To = new List<MailboxAddress>();
+
+            To.AddRange(to.Select(x => new MailboxAddress(x)));
+            Subject = subject;
+            Content = content;
+        }
+    }
+}
