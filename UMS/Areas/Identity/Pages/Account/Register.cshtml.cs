204ce8a64746e4e33db09ae409604038ac105fdb
Wannapa Srijermtong <wannapaa_s@hotmail.com>
2020-08-27 17:36:22 +0700

    Add RegularExpression in Register.cshtml.cs

--- UMS/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ UMS/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -65,11 +65,13 @@ namespace UMS.Areas.Identity.Pages.Account
             [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
             [DataType(DataType.Password)]
             [Display(Name = "Password")]
+            [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[$@$!%*?&])[A-Za-z0-9$@$!%*?&]+$"
+                ,ErrorMessage = "The password must contain at least <br> 1 uppercase, 1 lowercase, 1 digit and 1 special character.")]
             public string Password { get; set; }
 
             [DataType(DataType.Password)]
             [Display(Name = "Confirm password")]
-            [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+            [Compare("Password", ErrorMessage = "The password and confirm password do not match.")]
             public string ConfirmPassword { get; set; }
         }
 
