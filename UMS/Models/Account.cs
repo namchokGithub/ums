7c58b862d08980aa4aa34169b893173238c40e38
Wannapa Srijermtong <wannapaa_s@hotmail.com>
2020-08-27 09:16:42 +0700

    update

---
+++ UMS/Models/Account.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace UMS.Models
+{
+    public class Account
+    {
+        [Key]
+        [Required]
+        public string acc_Id { set; get; }
+
+        [Required]
+        public string acc_Firstname { set; get; }
+
+        [Required]
+        public string acc_Lastname { set; get; }
+    }
+}
