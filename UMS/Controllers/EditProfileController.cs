048e9aa936e8d96866fdc3bc03bf2fb5acb5bd7b
Wannapa Srijermtong <wannapaa_s@hotmail.com>
2020-08-28 10:43:07 +0700

    Select data from database for EditProfile

--- UMS/Controllers/EditProfileController.cs
+++ UMS/Controllers/EditProfileController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using UMS.Models;
 
 /*
  * Name: EditProfileController.cs
@@ -14,8 +15,16 @@ namespace UMS.Controllers
 {
     public class EditProfileController : Controller
     {
+        private readonly AccountContext _accountContext;
+        public EditProfileController(AccountContext accountContext)
+        {
+            _accountContext = accountContext;
+        }
         public IActionResult Index()
         {
+            var user = _accountContext.Account.FirstOrDefault<Account>();
+
+            ViewData["User"] = user;
             return View();
         }
     }
