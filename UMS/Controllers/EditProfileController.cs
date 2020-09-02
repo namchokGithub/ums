ad10e3eb2f12b0808fad6bee044ebdac5dfd6e1f
Wannapa Srijermtong <wannapaa_s@hotmail.com>
2020-09-02 12:17:24 +0700

    Update Validation for EditProfile

--- UMS/Controllers/EditProfileController.cs
+++ UMS/Controllers/EditProfileController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Linq;
 using System.Security.Cryptography;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
@@ -24,7 +25,7 @@ namespace UMS.Controllers
 
         /*
          * Name: EditProfileController
-         * Parameter: editprofileContext(EditProfileContext)
+         * Parameter: editprofileContext(EditProfileContext), signInManager(SignInManager<ApplicationUser>)
          * Author: Wannapa Srijermtong
          * Description: Set context for database
          */
@@ -62,12 +63,86 @@ namespace UMS.Controllers
         [HttpPost]
         public async Task<IActionResult> editProfile()
         {
+            //Get data from Form Input
             var acc_Id = HttpContext.Request.Form["acc_Id"];
             var acc_Firstname = HttpContext.Request.Form["acc_Firstname"];
             var acc_Lastname = HttpContext.Request.Form["acc_Lastname"];
             var acc_OldPassword = HttpContext.Request.Form["acc_OldPassword"];
             var acc_NewPassword = HttpContext.Request.Form["acc_NewPassword"];
+            var acc_ConfirmPassword = HttpContext.Request.Form["acc_ConfirmPassword"];
 
+            //Regular expression
+            var RegExName = @"^[a-zA-Z]+(([a-zA-Z])?[a-zA-Z]*)*$";
+            var RegExPassword = @"^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[$!%*?&])[A-Za-z0-9$!%*?&]+$";
+
+            // Validation if acc_Firstname do not math with Regular expression.
+            if (!Regex.IsMatch(acc_Firstname, RegExName))
+            {
+                // Toastr if Edit profile blank.
+                TempData["LoginErrorResult"] = @"toastr.warning('The Firstname must only character.')";
+
+                return RedirectToAction("Index", "EditProfile", new { id = acc_Id });
+            }
+
+            // Validation if acc_Lastname do not math with Regular expression.
+            if (!Regex.IsMatch(acc_Lastname, RegExName))
+            {
+                // Toastr if Edit profile blank.
+                TempData["LoginErrorResult"] = @"toastr.warning('The Lastname must only character.')";
+
+                return RedirectToAction("Index", "EditProfile", new { id = acc_Id });
+            }
+
+            // Validation if acc_OldPassword do not math with Regular expression.
+            if (!Regex.IsMatch(acc_OldPassword, RegExPassword))
+            {
+                // Toastr if Edit profile blank.
+                TempData["LoginErrorResult"] = @"toastr.warning('The password must contain at least <br> 1 uppercase, 1 lowercase, 1 digit and 1 special character.')";
+
+                return RedirectToAction("Index", "EditProfile", new { id = acc_Id });
+            }
+
+            // Validation if acc_NewPassword do not math with Regular expression.
+            if (!Regex.IsMatch(acc_NewPassword, RegExPassword))
+            {
+                // Toastr if Edit profile blank.
+                TempData["LoginErrorResult"] = @"toastr.warning('The password must contain at least <br> 1 uppercase, 1 lowercase, 1 digit and 1 special character.')";
+
+                return RedirectToAction("Index", "EditProfile", new { id = acc_Id });
+            }
+
+            // Validation if acc_ConfirmPassword do not math with Regular expression.
+            if (!Regex.IsMatch(acc_ConfirmPassword, RegExPassword))
+            {
+                // Toastr if Edit profile blank.
+                TempData["LoginErrorResult"] = @"toastr.warning('The password must contain at least <br> 1 uppercase, 1 lowercase, 1 digit and 1 special character.')";
+
+                return RedirectToAction("Index", "EditProfile", new { id = acc_Id });
+            }
+
+            // Validation if acc_Firstname, acc_Lastname, acc_OldPassword, acc_NewPassword and acc_ConfirmPassword is blank.
+            if (acc_Firstname == "" || acc_Lastname == "" || acc_OldPassword == "" || acc_NewPassword == "" || acc_ConfirmPassword == "")
+            {
+                // Validation if acc_Firstname and acc_Lastname is not blank.
+                if(acc_Firstname != "" && acc_Lastname != "")
+                {
+                    TempData["LoginSuccessResult"] = @"toastr.success('Edit profile successfully!')";
+
+                    // SQL text for execute procedure
+                    string sqlUpdateUser = $"update_User '{acc_Id}', '{acc_Firstname}', '{acc_Lastname}'";
+
+                    // Update Account
+                    _editprofileContext.Database.ExecuteSqlRaw(sqlUpdateUser);
+
+                    return RedirectToAction("Index", "EditProfile", new { id = acc_Id });
+                }
+                // Toastr if Edit profile blank.
+                TempData["LoginErrorResult"] = @"toastr.warning('The input can not be blank!')";
+
+                return RedirectToAction("Index", "EditProfile", new { id = acc_Id });
+            }
+
+            // Validation if acc_OldPassword is not blank.
             if (acc_OldPassword != "")
             {
                 var result = await _signInManager.PasswordSignInAsync(User.Identity.Name, acc_OldPassword, false, lockoutOnFailure: false);
@@ -111,7 +186,7 @@ namespace UMS.Controllers
             else
             {
                 // Toastr if Edit profile success.
-                TempData["LoginAllSuccessResult"] = @"toastr.success('Edit profile successfully!')";
+                TempData["LoginSuccessResult"] = @"toastr.success('Edit profile successfully!')";
 
                 // SQL text for execute procedure
                 string sqlUpdateUser = $"update_User '{acc_Id}', '{acc_Firstname}', '{acc_Lastname}'";
