be46b2c0ae4177edc0b1d7e32f15beaa6d39baa5
Wannapa Srijermtong <wannapaa_s@hotmail.com>
2020-09-03 12:03:59 +0700

    Update ResetPassword.cshtml.cs

--- UMS/Areas/Identity/Pages/Account/ResetPassword.cshtml.cs
+++ UMS/Areas/Identity/Pages/Account/ResetPassword.cshtml.cs
@@ -1,7 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
@@ -35,8 +33,9 @@ namespace UMS.Areas.Identity.Pages.Account
             public string Email { get; set; }
 
             [Required]
-            [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
             [DataType(DataType.Password)]
+            [StringLength(100
+                , ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
             [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[$@$!%*?&])[A-Za-z0-9$@$!%*?&]+$"
                 , ErrorMessage = "The password must contain at least <br> 1 uppercase, 1 lowercase, 1 digit and 1 special character.")]
             public string Password { get; set; }
