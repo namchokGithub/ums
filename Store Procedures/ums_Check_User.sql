90c61c2b8fc7061205e94de245c7466f660a0042
Namchok <pech4751@gmail.com>
2020-09-16 01:29:21 +0700

    ไม่แสดง User ตัวเองตอนจัดการผู้ใช้งาน

--- Store Procedures/ums_Check_User.sql
+++ Store Procedures/ums_Check_User.sql
@@ -9,8 +9,8 @@ AS
 BEGIN
     IF EXISTS (
 				SELECT [dbo].[Account].acc_Id
-    FROM [dbo].[Account]
-    WHERE [dbo].[Account].acc_User = @param_user AND [dbo].[Account].acc_IsActive = 'Y')
+                FROM [dbo].[Account]
+                WHERE [dbo].[Account].acc_User = @param_user AND [dbo].[Account].acc_IsActive = 'Y')
     BEGIN
         RETURN 1;
     END
