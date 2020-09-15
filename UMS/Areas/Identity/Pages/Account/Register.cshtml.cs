69385728c9cf801ad6a90f9a92ed7f78404f5b64
Wannapa Srijermtong <wannapaa_s@hotmail.com>
2020-09-15 17:55:51 +0700

    Update Regular expression

--- UMS/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ UMS/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -88,7 +88,7 @@ namespace UMS.Areas.Identity.Pages.Account
             [Display(Name = "Password")]
             [StringLength(100
                 , ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
-            [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[$@$!%*?&])[A-Za-z0-9$@$!%*?&]+$"
+            [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[$@$!%*#?&])[A-Za-z0-9$@$!%*#?&]+$"
                 ,ErrorMessage = "The password must contain at least <br> 1 uppercase, 1 lowercase, 1 digit and 1 special character.")]
             public string Password { get; set; }
 
