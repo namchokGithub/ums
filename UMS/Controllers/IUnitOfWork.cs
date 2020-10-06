faa122eb8fbb388c04204fc560abdb33da1742ae
namchokGithub <pech4751@gmail.com>
2020-10-06 12:29:38 +0700

    Add Unit of Work contoller

---
+++ UMS/Controllers/IUnitOfWork.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+/*
+ * Name: IUnitOfWork
+ * Author: Namchok Singhachai
+ * Description: interface class for control all.
+ */
+
+namespace UMS.Controllers
+{
+    public interface IUnitOfWork
+    {
+
+    } // End IUnitOfWork
+}
---
+++ UMS/Controllers/UnitOfWork.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+/*
+ * Name: IUnitOfWork
+ * Author: Namchok Singhachai
+ * Description: Class for control all.
+ */
+
+namespace UMS.Controllers
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+    } // End UnitOfWork
+}
