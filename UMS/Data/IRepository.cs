1bd6a4b341e342190bd55e4ffb8867d2cd4720da
namchokGithub <pech4751@gmail.com>
2020-10-06 11:52:45 +0700

    add IRepository

---
+++ UMS/Data/IRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+/*
+ * Name: IRepository
+ * Author: Namchok Singchai
+ * Description: For repository pattern.
+ */
+
+namespace UMS.Data
+{
+    public interface IRepository<T> where T : class
+    {
+        Task<T> Get(string id);
+        Task<T> Add(T entity);
+        Task<T> Update(T entity);
+        Task<T> Delete(string id);
+    } // End interface IRepository
+}
