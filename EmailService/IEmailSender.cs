1869a1817bedfb8297e2ec3ee26589abe5b68ddf
Namchok <pech4751@gmail.com>
2020-10-14 14:37:02 +0700

    update file

---
+++ EmailService/IEmailSender.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ * Name: IEmailSender
+ * Namspace: EmailService
+ * Author: Namchok Singhachai
+ * Description: Interface for send email
+ */
+
+namespace EmailService
+{
+    public interface IEmailSender
+    {
+        /*
+         * Name: SendEmail
+         * Parameter: message(Message)
+         * Author: Namchok Singhachai
+         * Description: Send email with your messages
+         */
+        void SendEmail(Message message); // End SendEmail
+
+        /*
+         * Name: SendEmailAsync
+         * Parameter: message(Message)
+         * Author: Namchok Singhachai
+         * Description: Send email with your messages (Async)
+         */
+        Task SendEmailAsync(Message message); // End SendEmailAsync
+    } // End IEmailSender
+}
