92e42b005176dbafc1a7689ad9626d6940c2773e
Namchok Singhachai <pech4751@gmail.com>
2020-10-16 17:05:26 +0700

    Update manage identity

--- UMS/Areas/Identity/Pages/Account/Manage/Index.cshtml.cs
+++ UMS/Areas/Identity/Pages/Account/Manage/Index.cshtml.cs
@@ -1,96 +1,80 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
+﻿using UMS.Models;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
 using UMS.Areas.Identity.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.ComponentModel.DataAnnotations;
+using System.Security.Claims;
+using System;
+using UMS.Data;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+/*
+ * Name: IndexModel.cs (extend: PageModel)
+ * Author: Idenity system
+ * Description: The page for manage account sites.
+ */
 
 namespace UMS.Areas.Identity.Pages.Account.Manage
 {
     public partial class IndexModel : PageModel
     {
+        private readonly ILogger<IndexModel> _logger;
+        private readonly AuthDbContext _context;
         private readonly UserManager<ApplicationUser> _userManager;
         private readonly SignInManager<ApplicationUser> _signInManager;
-
+        /*
+         * Name: IndexModel
+         * Parameter: logger(ILogger<AccessDeniedModel>)
+         */
         public IndexModel(
+            AuthDbContext context,
+            ILogger<IndexModel> logger,
             UserManager<ApplicationUser> userManager,
             SignInManager<ApplicationUser> signInManager)
         {
+            _logger = logger;
+            _context = context;
             _userManager = userManager;
             _signInManager = signInManager;
-        }
-
-        public string Username { get; set; }
-
-        [TempData]
-        public string StatusMessage { get; set; }
+        } // End constructor
 
         [BindProperty]
-        public InputModel Input { get; set; }
-
-        public class InputModel
-        {
-            [Phone]
-            [Display(Name = "Phone number")]
-            public string PhoneNumber { get; set; }
-        }
-
-        private async Task LoadAsync(ApplicationUser user)
-        {
-            var userName = await _userManager.GetUserNameAsync(user);
-            var phoneNumber = await _userManager.GetPhoneNumberAsync(user);
-
-            Username = userName;
-
-            Input = new InputModel
-            {
-                PhoneNumber = phoneNumber
-            };
-        }
+        public Management Input { get; set; }
 
         public async Task<IActionResult> OnGetAsync()
         {
-            var user = await _userManager.GetUserAsync(User);
-            if (user == null)
+            try
             {
-                return NotFound($"Unable to load user with ID '{_userManager.GetUserId(User)}'.");
-            }
-
-            await LoadAsync(user);
-            return Page();
-        }
-
-        public async Task<IActionResult> OnPostAsync()
-        {
-            var user = await _userManager.GetUserAsync(User);
-            if (user == null)
-            {
-                return NotFound($"Unable to load user with ID '{_userManager.GetUserId(User)}'.");
-            }
-
-            if (!ModelState.IsValid)
-            {
-                await LoadAsync(user);
+                _logger.LogTrace("Start index manamgement.");
+                _logger.LogTrace("Finding user ID.");
+                ViewData["UserId"] = User.FindFirstValue(ClaimTypes.NameIdentifier) ?? throw new Exception("The user ID not found !.");  // Get user ID
+                _logger.LogDebug("Getting all active users.");
+                ViewData["User"] = await GetAllAsync() ?? throw new Exception("Calling a method on a null object reference."); // Send data to view Index.cshtml
+                await _context.DisposeAsync();
+                _logger.LogTrace("End index manamgement.");
                 return Page();
             }
-
-            var phoneNumber = await _userManager.GetPhoneNumberAsync(user);
-            if (Input.PhoneNumber != phoneNumber)
+            catch (Exception e)
             {
-                var setPhoneResult = await _userManager.SetPhoneNumberAsync(user, Input.PhoneNumber);
-                if (!setPhoneResult.Succeeded)
-                {
-                    StatusMessage = "Unexpected error when trying to set phone number.";
-                    return RedirectToPage();
-                }
-            }
+                _logger.LogError(e.Message.ToString());
+                TempData["nullException"] = @"Swal.fire({ icon: 'error', title: 'Error !', text: `" + e.Message.Replace("`", "'").Replace("\\", "/") + @"`, showConfirmButton: true });";
+                _logger.LogTrace("End index manamgement.");
+                return Page();
+            } // End try catch
+        } // End On get Async
 
-            await _signInManager.RefreshSignInAsync(user);
-            StatusMessage = "Your profile has been updated";
-            return RedirectToPage();
-        }
-    }
+        /*
+         * Name: GetAllAsync
+         * Author: Namchok Singhachai
+         * Description: The getting list of all users.
+         */
+        public async Task<List<Management>> GetAllAsync()
+        {
+            return await _context.Management.FromSqlRaw("EXEC [dbo].ums_Get_all_active_user").ToListAsync<Management>();
+        } // End GetAllAsync
+    } // End index
 }
