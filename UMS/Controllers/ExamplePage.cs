60f260f01f94d7e9e3699072f8193d58c74fb2c0
Namchok Singhachai <pech4751@gmail.com>
2020-10-20 09:25:07 +0700

    Add example page controller and view

---
+++ UMS/Controllers/ExamplePage.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+/*
+ * Name: ExamplePage.cs
+ * Author: Namchok Singhachai
+ * Description: The controller page for authentication.
+ */
+
+namespace UMS.Controllers
+{
+    public class ExamplePage : Controller
+    {
+        private readonly ILogger<ExamplePage> _logger;
+        /*
+         * Name: AccountController
+         * Parameter: logger(ILogger<ExamplePage>)
+         */
+        public ExamplePage()
+        {
+            _logger.LogInformation("Start example page controller.");
+        } // End consturcter
+        /*
+         * Name: Index
+         * Author: Namchok Singhachai
+         */
+        public IActionResult Index()
+        {
+            return View();
+        } // End Index
+    } // End ExamplePage
+}
